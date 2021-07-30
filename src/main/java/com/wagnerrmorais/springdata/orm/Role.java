b1542b9d82bae097d0c13ca552838a923734661f
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-07-30 19:23:44 -0300

    Adding Update, view all and delete functionalities.

--- src/main/java/com/wagnerrmorais/springdata/orm/Role.java
+++ src/main/java/com/wagnerrmorais/springdata/orm/Role.java
@@ -27,4 +27,12 @@ public class Role {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/wagnerrmorais/springdata/service/RoleService.java
+++ src/main/java/com/wagnerrmorais/springdata/service/RoleService.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 @Service
 public class RoleService {
 
+    private Boolean system = true;
     private final RoleRepository roleRepository;
 
     public RoleService(RoleRepository roleRepository) {
@@ -16,7 +17,47 @@ public class RoleService {
     }
 
     public void init(Scanner scanner){
+        while (system){
+            System.out.println("Which Role action would you like to execute?");
+            System.out.println("0 - Exit");
+            System.out.println("1 - Save");
+            System.out.println("2 - Update");
+            System.out.println("3 - View all");
+            System.out.println("4 - Delete");
+
+            int action = scanner.nextInt();
+
+            switch (action){
+                case 0:
+                    system = false;
+                    break;
+                case 1:
+                    save(scanner);
+                    break;
+                case 2:
+                    update(scanner);
+                    break;
+                case 3:
+                    findAll();
+                    break;
+                case 4:
+                    deleteById(scanner);
+                    break;
+                default:
+                    system = false;
+                    break;
+            }
+
+        }
+
         save(scanner);
+    }
+
+    private void deleteById(Scanner scanner) {
+        System.out.println("Id");
+        int id = scanner.nextInt();
+        roleRepository.deleteById(id);
+        System.out.println("Deleted!");
     }
 
     public void save(Scanner scanner) {
@@ -26,6 +67,25 @@ public class RoleService {
         role.setDescription(description);
         roleRepository.save(role);
         System.out.println("Saved!");
+    }
+
+    public void update(Scanner scanner) {
+        System.out.println("Id:");
+        int id = scanner.nextInt();
+        System.out.println("Role description");
+        String description = scanner.next();
+
+        Role role = new Role();
+        role.setId(id);
+        role.setDescription(description);
+        roleRepository.save(role);
+        System.out.println("Updated!");
+    }
+
+    public void findAll() {
+        Iterable<Role> roles = roleRepository.findAll();
+        roles.forEach(System.out::println);
+        system = false;
     }
 
 }
