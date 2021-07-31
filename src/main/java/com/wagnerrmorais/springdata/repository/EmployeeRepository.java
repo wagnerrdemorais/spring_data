1267fe8c76ae209cf3cb08380616655490e0887d
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-07-30 21:06:29 -0300

    Adding Report funcionalityes

--- src/main/java/com/wagnerrmorais/springdata/repository/EmployeeRepository.java
+++ src/main/java/com/wagnerrmorais/springdata/repository/EmployeeRepository.java
@@ -1,12 +1,19 @@
 package com.wagnerrmorais.springdata.repository;
 
 import com.wagnerrmorais.springdata.orm.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 
     List<Employee> findByName(String name);
+
+//    List<Employee> findByNameAndSalaryGreaterThanAndStartingDate(String name, Double salary, LocalDate date);
+
+    @Query("SELECT e FROM Employee e WHERE e.name = :name AND e.salary >= :salary AND e.startingDate = :date")
+    List<Employee> findNameSalaryGreaterStartingDate(String name, Double salary, LocalDate date);
 
 }
--- src/main/java/com/wagnerrmorais/springdata/service/ReportService.java
+++ src/main/java/com/wagnerrmorais/springdata/service/ReportService.java
@@ -4,6 +4,8 @@ import com.wagnerrmorais.springdata.orm.Employee;
 import com.wagnerrmorais.springdata.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,6 +13,7 @@ import java.util.Scanner;
 public class ReportService {
 
     private Boolean system = true;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/aaaa");
 
     private final EmployeeRepository employeeRepository;
 
@@ -23,12 +26,16 @@ public class ReportService {
             System.out.println("Which Report action would you like to execute?");
             System.out.println("0 - Exit");
             System.out.println("1 - find employee by name");
+            System.out.println("1 - find employee by name");
 
             int action = scanner.nextInt();
 
             switch (action) {
                 case 1:
                     findEmployeeByName(scanner);
+                    break;
+                case 2:
+                    findEmployeeSalaryGTAndStartingDate(scanner);
                     break;
                 default:
                     system = false;
@@ -41,6 +48,22 @@ public class ReportService {
         System.out.println("Name to find: ");
         String name = scanner.next();
         List<Employee> list = employeeRepository.findByName(name);
+        list.forEach(System.out::println);
+    }
+
+    private void findEmployeeSalaryGTAndStartingDate(Scanner scanner) {
+        System.out.println("Name: ");
+        String name = scanner.next();
+
+
+        System.out.println("Starting date: ");
+        String startingDate = scanner.next();
+        LocalDate date = LocalDate.parse(startingDate, formatter);
+
+        System.out.println("Salary: ");
+        Double salary = scanner.nextDouble();
+
+        List<Employee> list = employeeRepository.findNameSalaryGreaterStartingDate(name, salary, date);
         list.forEach(System.out::println);
     }
 
